45ac345b85e52252ebf465c287f5d8bf8c4552bd
blksnk <art.jnveigel@gmail.com>
2020-02-20 23:37:40 +0100

    :sparkles: initLS function with listener support

--- src/helpers/layout.js
+++ src/helpers/layout.js
@@ -1,3 +1,5 @@
+import locomotiveScroll from "locomotive-scroll";
+
 export const wHeight = () => window.innerHeight;
 export const wWidth = () => window.innerWidth;
 export const rem = () =>
@@ -9,4 +11,18 @@ export const formatParagraphs = paragraph =>
 export const getCurrentScrollY = el => {
   const { y, top } = el.getBoundingClientRect();
   return Math.max(y, top);
+};
+
+export const initLS = (el, listener) => {
+  const ls = new locomotiveScroll({
+    el: el,
+    smooth: true,
+    smoothMobile: true,
+    inertia: 0.8,
+    getSpeed: true
+  });
+  if (listener) {
+    ls.on("scroll", listener);
+  }
+  return ls;
 };
